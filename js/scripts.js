198c657c2f506fc3c4ae36e5348099d917f0fcfe
Jarosław Terejko <j.terejko@gmail.com>
2017-08-15 11:08:32 +0200

    add files and folder to repository

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var name = prompt('Enter your name');
+// alert('Hello, ' + name); komentarz liniowy na poleceniu alert
+console.log('Hello, ' + name);
+
+
+// prosta matematyka
+var x = 2;
+var y = 3;
+var z = x + y;
+console.log(z);
+
+
+/*
+undefined komentarz blokowy
+*/
+var a; 
+console.log(a);
